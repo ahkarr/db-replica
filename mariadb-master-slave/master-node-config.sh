133011ef2c21b67862fd6f4a86b615f3c4b1538c
ahkarminmyat <dev.ahkarminmyat98@gmail.com>
2024-08-08 15:18:59 +0700

    config master node

---
+++ mariadb-master-slave/master-node-config.sh
@@ -0,0 +1,21 @@
+# configure node1 mariadb service port
+sudo ufw allow 3306
+
+sudo ufw allow 192.168.101.40 to any port 3306
+
+sudo vim /etc/mysql/mariadb.conf.d/50-server.cnf
+---
+bind-address = 0.0.0.0
+server-id =1
+---
+
+sudo vim /etc/mysql/my.cnf
+---
+[mariadb]
+bind-address = 0.0.0.0
+server-id = 1
+log_bin = mysql-bin
+---
+sudo systemctl restart mariadb.service
+
+sudo mysql_secure_installation
